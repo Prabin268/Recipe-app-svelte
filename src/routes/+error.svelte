378e3750b7d3789a520ed7f763468bdeaa952cab
Prabin268 <shresthaprabin540@gmail.com>
2026-03-25 11:37:35 +0545

    PWA without using workbox library1

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,28 +1,3 @@
-<!-- <script lang="ts">
-	import { page } from "$app/state";
-  
-    const displayStatus = page.status
-    
-    $:console.log("error status: ", displayStatus);
-</script>
-
-<svelte:head>
-  <title>{displayStatus} - Error</title>
-</svelte:head>
-
-<div class="h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900">
-  <div class="text-center space-y-4">
-    <h1 class="text-6xl font-bold text-gray-800 dark:text-white">{displayStatus}</h1>
-    <p class="text-lg text-gray-600 dark:text-gray-300">Not found</p>
-    <a 
-      href="/" 
-      class="inline-block mt-4 px-6 py-2 bg-blue-600 text-white rounded-xl shadow hover:bg-blue-700 transition"
-    >
-      Home
-    </a>
-  </div>
-</div> -->
-
 <script lang="ts">
 	import { page } from "$app/stores";
 
